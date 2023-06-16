8f3ba4dab6ee1331790f34958608603683d16b6a
vasumathi298 <vasumathi.n29@gmail.com>
2023-06-16 03:52:57 -0400

    mongoDB changes for auth and user

--- users/auth-controller.js
+++ users/auth-controller.js
@@ -9,7 +9,7 @@ const AuthController = (app) => {
             res.sendStatus(409);
             return;
         }
-        const newUser = await userDao.createUser(req.body);
+        const newUser = await usersDao.createUser(req.body);
         currentUserVar = newUser;
         res.json(newUser);
 
