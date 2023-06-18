d92964fa2bb03adf51e1c4dbcac824cfd5ae730e
Vasumathi Narayanan <72432533+vasumathi298@users.noreply.github.com>
2023-06-18 12:59:50 -0400

    Update users-dao.js

--- users/users-dao.js
+++ users/users-dao.js
@@ -1,20 +1,44 @@
-import usersModel from "./users-model.js";
+let users = [];
 
+export const findAllUsers = () => users;
 
+export const findUserById = (uid) => {
+    const index = users.findIndex((u) => u._id === uid);
+    if (index !== -1) return users[index];
+    return null;
+}
 
-export const findAllUsers = () =>
-  usersModel.find();
-export const findUserById = (id) =>
-  usersModel.findById(id);
-export const findUserByUsername = (username) =>
-  usersModel.findOne({ username });
-export const findUserByCredentials = (username, password) =>
-  usersModel.findOne({ username, password });
-export const createUser = (user) =>
-  usersModel.create(user);
-export const updateUser = (id, user) =>
-  usersModel.updateOne({ _id: id }, { $set: user });
-export const deleteUser = (id) =>
-  usersModel.deleteOne({ _id: id });
-export const findUserByHandle =(handle)=>
-  usersModel.findOne({ handle });
+export const findUserByUsername = (username) => {
+    const index = users.findIndex((u) => u.username === username);
+    if (index !== -1) return users[index];
+    return null;
+}
+
+export const findUserByHandle = (handle) => {
+    const index = users.findIndex((u) => u.handle === handle);
+    if (index !== -1) return users[index];
+    return null;
+}
+
+export const findUserByCredentials = (username, password) => {
+    const index = users.findIndex((u) => u.username === username && u.password === password);
+    if (index !== -1) return users[index];
+    return null;
+}
+
+export const createUser = (user) => { user._id = (new Date()).getTime() + ''; users.push(user); return user;};
+
+export const updateUser = (uid, user) => {
+    console.log("user id to update is ",uid);
+
+    const index = users.findIndex((u) => u._id === uid);
+    users[index] = { ...users[index], ...user};
+    console.log(users[index]);
+    return users[index];
+}
+
+export const deleteUser = (uid) => {
+    const index = users.findIndex((u) => u._id === uid);
+    users.splice(index, 1);
+    return {status: "ok"};
+}
