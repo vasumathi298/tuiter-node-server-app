d56e3f254fabd87d1963ccde712c5670fba66f66
Vasumathi Narayanan <72432533+vasumathi298@users.noreply.github.com>
2023-06-18 13:02:11 -0400

    Update tuits-controller.js

--- controllers/tuit/tuits-controller.js
+++ controllers/tuit/tuits-controller.js
@@ -1,37 +1,33 @@
 import posts from "./tuits.js";
 let tuits = posts;
 
-import * as tuitsDao from './tuits-dao.js'
-
-const createTuit = async (req, res) => {
+const createTuit = (req, res) => {
     const newTuit = req.body;
+    newTuit._id = (new Date()).getTime()+'';
     newTuit.likes = 0;
     newTuit.liked = false;
-    const insertedTuit = await tuitsDao.createTuit(newTuit);
-    res.json(insertedTuit);
+    tuits.push(newTuit);
+    res.json(newTuit);
   }
-  
-  const findTuits = async (req, res) => {
-    const tuits = await tuitsDao.findTuits()
-    res.json(tuits);
- }
- 
 
-const deleteTuit = async (req, res) => {
+const findTuits = (req, res) =>
+   res.json(tuits);
+
+const deleteTuit = (req, res) => {
     const tuitdIdToDelete = req.params.tid;
-    const status = await tuitsDao.deleteTuit(tuitdIdToDelete);
-    res.json(status);
-
-  }
-  const updateTuit = async (req, res) => {
-    const tuitdIdToUpdate = req.params.tid;
-    const updates = req.body;
-    const status = await tuitsDao.updateTuit(tuitdIdToUpdate, updates);
-    res.json(status);
+    tuits = tuits.filter((t) =>
+      t._id !== tuitdIdToDelete);
     res.sendStatus(200);
   }
-  
-  
+  const updateTuit = (req, res) => {
+    const tuitdId = req.params.tid;
+    const updates = req.body;
+    const tuitIndex = tuits.findIndex((t) => t._id === tuitdId)
+    tuits[tuitIndex] = {...tuits[tuitIndex], ...updates};
+    res.sendStatus(200);
+  }
+
+
 const TuitsController = (app) => {
  app.post('/api/tuits', createTuit);
  app.get('/api/tuits', findTuits);
